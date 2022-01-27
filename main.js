dedecec96386b4453c5af05071fbfe464418ca7e
Manuel <manuel@pop-os.localdomain>
2022-01-27 20:31:38 -0300

    Add DisplayController module

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const playerFactory = function (name, symbol) {
   return {
     name,
@@ -9,26 +10,95 @@ const playerFactory = function (name, symbol) {
 const player1 = new playerFactory('Player 1', 'X');
 const player2 = new playerFactory('Player 2', 'O');
 
+const DisplayController = (function () {
+  function _reassignPlayerName() {
+    const submitNameBtns = document.querySelectorAll('.submit-name');
+
+    submitNameBtns.forEach((btn) => {
+      btn.addEventListener('click', function (e) {
+        let displayedPlayerName = e.target.parentElement.children[0];
+        let inputFieldName = e.target.parentElement.children[1];
+
+        if (inputFieldName.value !== '' && inputFieldName !== ' ') {
+          displayedPlayerName.textContent === player1.name
+            ? (player1.name = inputFieldName.value)
+            : (player2.name = inputFieldName.value);
+
+          displayedPlayerName.textContent = inputFieldName.value;
+        }
+
+        inputFieldName.value = '';
+      });
+    });
+  }
+
+  function _toggleClassOnModal() {
+    let modalBackground = document.querySelector('.modal-bg');
+    modalBackground.classList.toggle('modal-active');
+  }
+
+  function displayModal(winner) {
+    if (winner) {
+      _toggleClassOnModal();
+    }
+    if (winner === null) {
+      _toggleClassOnModal();
+    }
+    if (winner === false) {
+      _toggleClassOnModal();
+    }
+
+    _updateDisplayedWinner(winner);
+  }
+
+  function _updateDisplayedWinner(winner) {
+    let modalMessage = document.querySelector('#modal-message');
+    if (winner) {
+      modalMessage.textContent = `${winner} won the game!`;
+    } else {
+      modalMessage.textContent = "It's a tie!";
+    }
+  }
+
+  function _closeModal() {
+    let closeBtn = document.querySelector('.close-modal');
+
+    closeBtn.addEventListener('click', function (e) {
+      _toggleClassOnModal();
+    });
+  }
+
+  _reassignPlayerName();
+  _closeModal();
+
+  return {
+    displayModal,
+  };
+})();
+
 const Gameboard = (function () {
   let _boardArr = new Array(9).fill('');
   let _cells = Array.from(document.querySelectorAll('.cell'));
   let _turnCount = 1;
   let _winner = undefined;
 
-  // Restart variables related to functionality and cell elements.
   function _restartGame() {
-    const restartBtn = document.querySelector('.restart');
-    restartBtn.addEventListener('click', function () {
-      _boardArr.fill('');
-      _turnCount = 1;
-      _winner = undefined;
-      _cells.forEach((cell) => {
-        cell.textContent = '';
+    const restartBtn = Array.from(document.querySelectorAll('.restart'));
+
+    restartBtn.forEach((restartBtn) => {
+      restartBtn.addEventListener('click', function () {
+        DisplayController.displayModal(_winner);
+        _boardArr.fill('');
+        _turnCount = 1;
+        _winner = undefined;
+        _cells.forEach((cell) => {
+          cell.textContent = '';
+        });
+        // DisplayController.displayModal();
       });
     });
   }
 
-  // Depending on _turnCount, return player.symbol
   function _checkTurn() {
     if (_turnCount % 2 === 1) {
       _turnCount++;
@@ -39,12 +109,9 @@ const Gameboard = (function () {
     }
   }
 
-  // Check if there is a winner, add symbol to board and
-  // add corresponding element to _boardArr
   function _addSymbol() {
     _cells.forEach((cell) => {
       cell.addEventListener('click', function (e) {
-        _checkWinner();
         if (!_winner) {
           if (cell.textContent === '' && _turnCount < 10) {
             let cellIdx = +e.target.dataset.cell;
@@ -52,13 +119,13 @@ const Gameboard = (function () {
             _boardArr.splice(cellIdx, 1, cell.textContent);
           }
         }
+
+        _checkWinOrTie();
       });
     });
   }
 
-  // Start to check after _turnCount > 5, check for winning conditions
-  // and return winner.
-  function _checkWinner() {
+  function _checkWinOrTie() {
     if (_turnCount > 5) {
       const winningConditions = [
         [0, 1, 2],
@@ -70,6 +137,7 @@ const Gameboard = (function () {
         [0, 4, 8],
         [2, 4, 6],
       ];
+
       for (let i = 0; i <= 7; i++) {
         let winCondition = winningConditions[i];
         let optA = _boardArr[winCondition[0]];
@@ -83,6 +151,11 @@ const Gameboard = (function () {
         }
       }
     }
+    if (_turnCount === 9) {
+      _winner = null;
+    }
+
+    return DisplayController.displayModal(_winner);
   }
 
   _restartGame();
